a14a44a571026fc31f43017875f420095f4e0a40
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2024-07-27 21:30:23 -0300

    Update useAulas.ts

--- src/core/hook/useAulas.ts
+++ src/core/hook/useAulas.ts
@@ -3,31 +3,29 @@ import { useEffect, useState } from "react"
 import Aula from "@/core/model/Aula" /** Modelo Importado  */ 
 import listaDeAulas from "../data/listaDeAulas" /** Dados mocado  */ 
 
-export default function useAulas(){
-    const [aulas, setAulas] = useState<Aula[]>(listaDeAulas)
-    const [duracaoTotal, setDuracaoTotal] = useState<number>(0)
+export default function useAulas(){ /** Criando Nosso Hook  */ 
+    const [aulas, setAulas] = useState<Aula[]>(listaDeAulas) /** Criando nosso controle de lista */ 
+    const [duracaoTotal, setDuracaoTotal] = useState<number>(0) /** Criando nosso estado e inicializando com 0 */ 
 
-    function filtrarAulas(status?: string | null){
-        
+    function filtrarAulas(status?: string | null){ /** Criando nosso filtro  */ 
         const aulasFiltradas: Aula[] = listaDeAulas.filter( 
             (aula) => aula.status === status || !status 
         )
-        setAulas(aulasFiltradas)
-        
+        setAulas(aulasFiltradas) /** Setando o resultado do filtro  */ 
     }
 
-    useEffect(() => {
+    useEffect(() => { /** Carregando nassa lista criada no estado  */ 
         calcularDuracaoTotal(aulas)
     }, [aulas])
 
-    function calcularDuracaoTotal(aulas: Aula[]){
+    function calcularDuracaoTotal(aulas: Aula[]){ /** Calculando os itens de uma lista  */ 
         const duracaoTotal = aulas.reduce((acc, aula) => acc + aula.duracao, 0)
         setDuracaoTotal(duracaoTotal)
     }
 
-    return{
-        aulas,
-        duracaoTotal,
-        filtrarAulas
+    return{ 
+        aulas, /** retorna a lista state  */ 
+        duracaoTotal, /** retorna total em uma lista */ 
+        filtrarAulas /** retorna a lista filtrada  */ 
     }
 }

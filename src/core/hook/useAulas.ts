ec65c28b651bb02f9dce3bb17e1ad094e51ca078
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2024-07-27 21:32:12 -0300

    Update useAulas.ts

--- src/core/hook/useAulas.ts
+++ src/core/hook/useAulas.ts
@@ -1,31 +1,27 @@
 import { useEffect, useState } from "react"
-
 import Aula from "@/core/model/Aula" /** Modelo Importado  */ 
 import listaDeAulas from "../data/listaDeAulas" /** Dados mocado  */ 
 
 export default function useAulas(){ /** Criando Nosso Hook  */ 
-    const [aulas, setAulas] = useState<Aula[]>(listaDeAulas) /** Criando nosso controle de lista */ 
-    const [duracaoTotal, setDuracaoTotal] = useState<number>(0) /** Criando nosso estado e inicializando com 0 */ 
+    const [aulas, setAulas] = useState<Aula[]>(listaDeAulas) /** 1. Criando nosso controle de lista */ 
+    const [duracaoTotal, setDuracaoTotal] = useState<number>(0) /** 2. Criando nosso estado e inicializando com 0 */ 
 
-    function filtrarAulas(status?: string | null){ /** Criando nosso filtro  */ 
+    function filtrarAulas(status?: string | null){ /** 3. Criando nosso filtro  */ 
         const aulasFiltradas: Aula[] = listaDeAulas.filter( 
             (aula) => aula.status === status || !status 
         )
         setAulas(aulasFiltradas) /** Setando o resultado do filtro  */ 
     }
-
-    useEffect(() => { /** Carregando nassa lista criada no estado  */ 
+    useEffect(() => { /** 4. Carregando nassa lista criada no estado  */ 
         calcularDuracaoTotal(aulas)
     }, [aulas])
-
-    function calcularDuracaoTotal(aulas: Aula[]){ /** Calculando os itens de uma lista  */ 
+    function calcularDuracaoTotal(aulas: Aula[]){ /** 5. Calculando os itens de uma lista  */ 
         const duracaoTotal = aulas.reduce((acc, aula) => acc + aula.duracao, 0)
         setDuracaoTotal(duracaoTotal)
     }
-
     return{ 
-        aulas, /** retorna a lista state  */ 
-        duracaoTotal, /** retorna total em uma lista */ 
-        filtrarAulas /** retorna a lista filtrada  */ 
+        aulas, /** 6. retorna a lista state  */ 
+        duracaoTotal, /** 7. retorna total em uma lista */ 
+        filtrarAulas /** 8. retorna a lista filtrada  */ 
     }
 }
